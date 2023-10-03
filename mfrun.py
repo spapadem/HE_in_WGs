8b0572f4703224458fa07719d723d0bb122e09ff
Symeon Papadimitropoulos <simpapadem@gmail.com>
2023-10-03 09:44:35 -0700

    Cleaning up / File renames

--- mfrun.py
+++ mfrun.py
@@ -10,8 +10,8 @@ for frq in frequencies:
     file = open('freq_used.py', 'w')
     file.write("frq= " + str(frq))
     file.close()
-    os.system("python3 HEWG_inc_vard.py")
-    os.system("python3 HEWG_tot_alt.py")
+    os.system("python3 HEWG_inc_var_depth.py")
+    os.system("python3 HEWG_tot_var_depth.py")
 # 
 # for frq in frequencies:
 #     print("Working on frequency "+ str(frq))
