5e964086660807e07c12a7fb1ae62c109d65a8a2
Symeon Papadimitropoulos <simpapadem@gmail.com>
2023-12-11 10:48:47 -0800

    fixed

--- meshing_circle.py
+++ meshing_circle.py
@@ -11,49 +11,35 @@ import ngsolve.internal as ngsint
 
 
 # Creating the waveguide geometry.
-geo = SplineGeometry()
+# geo = SplineGeometry()
 
-Nbn = 151
-n = 1.5
-t = np.linspace(0,2*pi,Nbn)
-t = t[0:-1]
-rvals = np.abs(np.cos(n*t))**(np.sin(2*n*t))
-
-x = rvals*np.cos(t)
-y = rvals*np.sin(t)
-
-P = np.zeros((t.shape[0],2))
-P[:,0] = x
-P[:,1] = y
-pnts = [tuple(x) for x in P]
-
-pp = [geo.AppendPoint(*pnt) for pnt in pnts]
-curves = []
-for i in range(Nbn-2):
-    curves.append([["line",pp[i],pp[i+1]],"round"])
-curves.append([["line",pp[-1],pp[0]],"round"])
-[geo.Append(c,bc=bc) for c,bc in curves]
-# pnts = [pnt for pnt in (x,y)]
-
-# geo2 = SplineGeometry()
-# p1,p2,p3,p4,p5,p6,p7,p8,p9 = [geo2.AppendPoint(*pnt) for pnt in pnts]
-# curves2 = [[["line",p1,p2],"top"],
+# pnts =[(0,0), #1
+#        (Wm,0), #2
+#        (Wm,Dm),  #3
+#        (Wm-4*lambda_0,Dm), #4
+#        (Wm-7*lambda_0,Dm), #5
+#        (0.5*(Wc+Wm-5*lambda_0), (Dc+Dm)/2), #6
+#        (Wc,Dc), #7
+#        (Wc-2*lambda_0,Dc), #8
+#        (0,Dc)] #9
+# p1,p2,p3,p4,p5,p6,p7,p8,p9 = [geo.AppendPoint(*pnt) for pnt in pnts]
+# curves = [[["line",p1,p2],"top"],
 #           [["line",p2,p3],"right"],
 #           [["line",p3,p4],"bottom"],
 #           [["spline3",p4,p5,p6],"bottom"],
 #           [["spline3",p6,p7,p8],"bottom"],
 #           [["line",p8,p9],"bottom"],
 #           [["line",p9,p1],"left"]]
-# [geo2.Append(c,bc=bc) for c,bc in curves]
+# [geo.Append(c,bc=bc) for c,bc in curves]
 
 # Creating the waveguide geometry.
-# WG = MoveTo(0,0).Circle(0, 0, 1).Face() # Creating the rectangle of size WxD, and a circular scatterer at (x_sc,y_sc), of size b.
+WG = MoveTo(0,0).Circle(0, 0, 1).Face() # Creating the rectangle of size WxD, and a circular scatterer at (x_sc,y_sc), of size b.
 # PMLOUT = MoveTo(0,0).Circle(0, 0, 2).Face()
 
 #Combining the 3 domains.
-# geo = OCCGeometry(WG, dim=2) 
+geo = OCCGeometry(WG, dim=2) 
 
-# geo.SetMaterial(1,"round")
+# geo.SetMaterial(2,"PMLL")
 # geo.SetMaterial(3,"PMLR")
 mesh = Mesh(geo.GenerateMesh(maxh=0.1))
 mesh.Curve(3)
